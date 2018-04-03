61fb91d25d3c54de96edfcdc475d97bdaed55120
jithinks <ks.jithin@gmail.com>
2018-04-03 00:25:57 -0400

    Test cases

--- src/main/java/com/jks/startup/springbootsecurity/security/SecurityConfig.java
+++ src/main/java/com/jks/startup/springbootsecurity/security/SecurityConfig.java
@@ -3,9 +3,11 @@ package com.jks.startup.springbootsecurity.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     // Authentication : User --> Roles
--- src/test/java/com/jks/startup/springbootsecurity/SpringbootSecurityApplicationTests.java
+++ src/test/java/com/jks/startup/springbootsecurity/SpringbootSecurityApplicationTests.java
@@ -1,16 +1,61 @@
 package com.jks.startup.springbootsecurity;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = RANDOM_PORT, classes = SpringbootSecurityApplication.class)
 public class SpringbootSecurityApplicationTests {
 
 	@Test
 	public void contextLoads() {
 	}
 
+	TestRestTemplate restTemplate;
+	URL base;
+	@LocalServerPort int port;
+
+	@Before
+	public void setUp() throws MalformedURLException {
+		restTemplate = new TestRestTemplate("jithin", "jithin");
+		base = new URL("http://localhost:" + port+"/startup/");
+	}
+
+	@Test
+	public void whenLoggedUserRequestsHomePage_ThenSuccess()throws IllegalStateException, IOException {
+		ResponseEntity<String> response = restTemplate.getForEntity(base.toString(), String.class);
+
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertTrue(response
+				.getBody()
+				.contains("Jack"));
+	}
+
+	@Test
+	public void whenUserWithWrongCredentials_thenUnauthorizedPage()
+			throws Exception {
+
+		restTemplate = new TestRestTemplate("jithin", "jithin-wrongpassword");
+		ResponseEntity<String> response = restTemplate.getForEntity(base.toString(), String.class);
+
+		assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+		/*assertTrue(response
+				.getBody()
+				.contains("Unauthorized"));*/
+	}
 }
